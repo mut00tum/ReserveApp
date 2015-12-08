be5820b4dec37875410a21151d0101eb12e383eb
mut00tum <mmuuttuu@gmail.com>
2015-12-08 20:55:47 +0900

    pre reserveCard

--- src/files/js/m/reserve.js
+++ src/files/js/m/reserve.js
@@ -1,14 +1,19 @@
-var m       = require( 'mithril' );
+var m  = require( 'mithril' );
 
 module.exports = function ReserveModule() {
   // モデルクラス
   var Reserve = function ( data ) {
     this.position = m.prop( data.position );
+    this.place    = m.prop( data.place );
+    this.hour     = m.prop( data.hour );
+    this.member   = m.prop( data.member );
+    this.person   = m.prop( data.person );
   }
 
     //クラス
     Reserve.save = function( reserve ) {
-      localStorage.setItem( "position", JSON.stringify( reserve ) );
+      // console.log( 'save:' + JSON.stringify(reserve) )
+      localStorage.setItem( "reserved", JSON.stringify( reserve ) );
     }
 
     //クラス ：localStorageに保存してある文字列→JSON
@@ -18,16 +23,16 @@ module.exports = function ReserveModule() {
         booking = [],
         str , obj;
 
-      str = localStorage.getItem( "position" );
+      str = localStorage.getItem( "reserved" );
+      // console.log( 'load:' + str);
 
       if (str) {
           //localStorage から get した文字列をJSONに。
           var json = JSON.parse(str);
 
           for (var i = 0; i < json.length; i++) {
-              booking.push( new Reserve( json[i] ));
+            booking.push( new Reserve( json[i] ) );
           }
-          // console.log(booking[0].position())
       }
 
     return m.prop( booking );
@@ -44,66 +49,62 @@ module.exports = function ReserveModule() {
       vm.date     = m.prop('');
       vm.time     = m.prop('');
       vm.hour     = m.prop('');
-      vm.people   = m.prop('');
-      vm.name     = m.prop('');
+      vm.member   = m.prop('');
+      vm.person   = m.prop('');
           
       vm.clickSubmitButton = function(){
 
         var
-          reserve , position;
+          reserve = [],
+          position , place , hour , member , person;
 
         // vm.position に getPosition() を格納
         vm.position( vm.getPosition() );
 
-        //モデルの position を vm.position で初期化
-        reserve = new Reserve({ position : vm.position() });
-        // console.log(reserve);
+        //モデルの プロパティ を 初期化
+        reserve = new Reserve({ 
+          position : vm.position(),
+          place    : vm.place(),
+          hour     : vm.hour(),
+          member   : vm.member(),
+          person   : vm.person()
+        });
 
         //※「インスタンス化したモデル」をpushする
         vm.list().push( reserve );
-
-        vm.position( '' );
-        Reserve.save( vm.list() )
         
+        vm.position( '' );
+        vm.place( '' );
+        vm.hour( '' );
+        vm.member( '' );
+        vm.person( '' );
+
+        Reserve.save( vm.list() )        
       },
       vm.getPosition = function(){
         var
           position = '';
 
-        position = vm.date() + '_' + vm.time() + '_' + vm.place();
+        position = vm.date() + '_' + vm.time();
 
         return position;
-      },
-      vm.getHour = function() {
-        var
-          hour = vm.hour();
-
-        return hour;
-      },
-      vm.getMemberInfo = function(){
-        var
-          member = '';
-
-        member = vm.people() + '_' + vm.name();
-
-        return member;
       },
       vm.getReserveList = function(){
 
         var 
-          data = localStorage.getItem( "position" ),
+          data = localStorage.getItem( "reserved" ),
           list  = JSON.parse( data ),
           reserveList = [],
-          prop;
+          position , place , prop;
 
         if ( data ) {
-
           var length = list.length;
           for( var i = 0; i < length; i++ ) {
-            prop = list[i].position;
+            position = list[i].position;
+            place    = list[i].place;
+            prop     = position + '/' + place;
             reserveList.push( prop );
           }
-          // console.log( list[0].position );
         }
 
         return reserveList;
@@ -111,8 +112,10 @@ module.exports = function ReserveModule() {
       }
     },
     test : function(){
-       // console.log(localStorage.getItem('position'))
-       console.log( vm.getReserveList() );
+       console.log(localStorage.getItem('reserved'))
+       // console.log( vm.getReserveList() );
+       // vm.getReserveList()
+       // Reserve.list()
 
     },
     clear : function() {
@@ -131,13 +134,13 @@ module.exports = function ReserveModule() {
           arr = vm.date().split('_');
         return arr;
       },
-      vm.getWeekDay = function( d ) {
+      vm.setWeekDay = function( d ) {
         var weekArr = [
           'sun' , 'mon', 'tue' , 'wed' , 'thu' , 'fri' , 'sat'
         ];
         return weekArr[d];
       },
-      vm.getWeekArr = function( dObj ) {
+      vm.setWeekArr = function( dObj ) {
         var
           term = 63,  // ▼ CSS：#days ul,#times ul{ width }
           arr  = [],
@@ -154,14 +157,14 @@ module.exports = function ReserveModule() {
         }
         return arr;
       },
-      vm.getTimeArea = function() {
+      vm.setTimeArea = function() {
         return [
           '0800','0830','0900','0930','1000','1030','1100','1130','1200','1230',
           '1300','1330','1400','1430','1500','1530','1600','1630','1700','1730',
           '1800','1830','1900','1930','2000','2030'
         ];
       },
-      vm.getPlaces = function () {
+      vm.setPlaces = function () {
         return [
           '9F_A','9F_B','1F_A','1F_B'
         ];
@@ -209,17 +212,40 @@ module.exports = function ReserveModule() {
         var
           Class = '',
           idArr = vm.getReserveList(),
-          calendarId = addDateClass( d ) + '_' + t + '_' + p,
-          length;
+          calendarId = addDateClass( d ) + '_' + t + '/' + p,
+          length , place;
 
         length = idArr.length;
 
         Class = calendarId;
         for( var i = 0; i < length; i++ ){
           if ( idArr[i] == calendarId ){
-            Class = 'reserved' + ' ' + calendarId;
+            
+            place = splitText( idArr[i] );
+            // console.log(place)
+            switch( place ) {
+              case '9F_A':
+                Class = 'reserved' + ' ' + 'P9A' + ' ' + calendarId;
+                break;
+              case '9F_B':
+                Class = 'reserved' + ' ' + 'P9B' + ' ' + calendarId;
+                break;
+              case '1F_A':
+                Class = 'reserved' + ' ' + 'P1A' + ' ' + calendarId;
+                break;
+              case '1F_B':
+                Class = 'reserved' + ' ' + 'P1B' + ' ' + calendarId;
+                break;
+            }
           } 
         }
+
+        function splitText( str ) {
+          var arr   = str.split('/');
+          var place = arr[1];
+          return place;
+        }
+
         return Class;
 
       }
@@ -237,23 +263,23 @@ module.exports = function ReserveModule() {
         m( 'button#test' , { onclick: vm.clear } , 'clear' ),
         m( '#calendar' , [
           m( '#days' , [
-            m( 'ul' , vm.getWeekArr( vm.dObj ).map(function ( d ) {
+            m( 'ul' , vm.setWeekArr( vm.dObj ).map(function ( d ) {
               return  m( 'li' , [
                   m( 'p' , [
                     m( 'span.date' , d.getDate() ),
-                    m( 'span.day' , vm.getWeekDay(d.getDay()) ),
+                    m( 'span.day' , vm.setWeekDay(d.getDay()) ),
                   ])
                 ]);
               }))
           ]),
           m( '#times' , [
-            m( 'ul#uiTimesList' , vm.getWeekArr( vm.dObj ).map(function ( d ) {
+            m( 'ul#uiTimesList' , vm.setWeekArr( vm.dObj ).map(function ( d ) {
               return  m( 'li' , {
                 onclick: m.withAttr( 'name' , vm.date ),
                 class  : addTodayClass( d ),
                 name   : addDateClass( d )
               } , [
-                m( '.timeArea' , vm.getTimeArea().map(function ( t ) {
+                m( '.timeArea' , vm.setTimeArea().map(function ( t ) {
                   return  m( 'ul.time' , {
                     onclick: addMultiProp(
                       m.withAttr( 'name' , vm.time )
@@ -261,7 +287,7 @@ module.exports = function ReserveModule() {
                       ),
                     name: t
                     } ,
-                    vm.getPlaces().map(function ( p ) {
+                    vm.setPlaces().map(function ( p ) {
                       return  m( 'li.place' , {
                         onclick : m.withAttr( 'name' , vm.place ),
                         // id      : addDateClass( d ) + '_' + t + '_' + p,
@@ -274,54 +300,58 @@ module.exports = function ReserveModule() {
             })),
           ])          
         ]),
-        m( 'form#reserve' , [
-          m('h2', 'reserve'),
+        m( 'form#reserveCard' , [
+          m('h2', '予約カード'),
           m('ul', [
-            // m('li', [
-            //   m('h3', '場所'),
-            //   m( 'label' , [
-            //     m( "input[type='radio'][name='area'][value='9FA']" , {onclick: m.withAttr( "value", vm.place)})
-            //   ] , '9F/大' ),
-            //   m( 'label' , [
-            //     m( "input[type='radio'][name='area'][value='9FB']" , {onclick: m.withAttr( "value", vm.place)})
-            //   ] , '9F/小' ),
-            //   m( 'label' , [
-            //     m( "input[type='radio'][name='area'][value='1FA']" , {onclick: m.withAttr( "value", vm.place)})
-            //   ] , '1F/窓口側' ),
-            //   m( 'label' , [
-            //     m( "input[type='radio'][name='area'][value='1FB']" , {onclick: m.withAttr( "value", vm.place)})
-            //   ] , '1F/コンビニ側' )
+            m('li.place', [
+              m('h3', 'Place'),
+              m( 'p.val' , vm.place() )
+            ]),
+            m( 'li', [
+              m( 'h3' , 'Date' ),
+              m( 'p.val' , vm.setTargetDate() )
+            ]),
+            m('li.place', [
+              m('h3', 'Time'),
+              m( 'p.val' , vm.time() )
+            ]),
+            // m( 'li', [
+            //   m( 'h3', 'Hour' ),
+            //   m( "select.val", {onchange: m.withAttr( "value", vm.hour)}, [
+            //     m( "option", '0' ),
+            //     m( "option[value='0.5']", '0.5' ),
+            //     m( "option[value='1']", '1' ),
+            //     m( "option[value='1.5']", '1.5' ),
+            //     m( "option[value='2']", '2' ),
+            //     m( "option[value='2.5']", '2.5' ),
+            //     m( "option[value='3']", '3' ),
+            //     m( "option[value='3.5']", '3.5' ),
+            //     m( "option[value='4']", '4' ),
+            //     m( "option[value='4.5']", '4.5' ),
+            //     m( "option[value='5']", '5' ),
+            //     m( "option[value='8']", '終日' )
+            //   ]),
+            //   m( 'p', 'h' )
             // ]),
             m( 'li', [
-              m( 'h3' , '日にち' ),
-              m( 'p#reserveDate' , vm.setTargetDate() )
+              m( 'h3', 'Hour' ),
+              m( '.wrap' , [
+                m( "input[name='num'][size='2'][type='number'][step='0.5'][min='0.5'][max='8']" , 
+                  {onchange: m.withAttr( "value", vm.hour )} ),
+                m( 'p.unit', 'h' )
+              ])
             ]),
             m( 'li', [
-              m( 'h3', '使用時間' ),
-              m( "select", {onchange: m.withAttr( "value", vm.hour)}, [
-                m( "option", '0' ),
-                m( "option[value='0.5']", '0.5' ),
-                m( "option[value='1']", '1' ),
-                m( "option[value='1.5']", '1.5' ),
-                m( "option[value='2']", '2' ),
-                m( "option[value='2.5']", '2.5' ),
-                m( "option[value='3']", '3' ),
-                m( "option[value='3.5']", '3.5' ),
-                m( "option[value='4']", '4' ),
-                m( "option[value='4.5']", '4.5' ),
-                m( "option[value='5']", '5' ),
-                m( "option[value='8']", '終日' )
-              ]),
-              m( 'p', '時間' )
+              m( 'h3', 'Member' ),
+              m( '.wrap' , [
+                m( "input[name='num'][size='2'][type='number'][min='1'][max='20']" , 
+                  {onchange: m.withAttr( "value", vm.member )} ),
+                m( 'p.unit', '人' )
+              ])              
             ]),
             m( 'li', [
-              m( 'h3', '人数' ),
-              m( "input[name='num'][size='2'][type='number'][min='1'][max='20']" , {onchange: m.withAttr( "value", vm.people)} ),
-              m( 'p', '名' )
-            ]),
-            m( 'li', [
-              m( 'h3', '名前' ),
-              m( "input[name='name'][size='10'][type='text']" , {onchange: m.withAttr( "value", vm.name)} )
+              m( 'h3', 'Name' ),
+              m( "input.val[name='name'][size='10'][type='text']" , {onchange: m.withAttr( "value", vm.person)} )
             ])
           ]),
           m( 'a#submit' , { onclick: vm.clickSubmitButton } , '予約する' )
